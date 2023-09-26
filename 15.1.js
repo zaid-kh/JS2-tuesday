ff5875b89000ae35c699317754d32a355fcb688f
zaid-kh <z.kh2800@gmail.com>
2023-09-26 16:48:40 +0300

    exe 15.1

---
+++ 15.1.js
@@ -0,0 +1,11 @@
+function getSum(arr1, arr2) {
+  let sum = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    sum += arr1[i];
+  }
+  for (let i = 0; i < arr2.length; i++) {
+    sum += arr2[i];
+  }
+  return sum;
+}
+alert("sum = " + getSum([1, 2, 3], [5, 66, 23]));
